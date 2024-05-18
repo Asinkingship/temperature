c77248b73ffb31fcafa0df58ef91b074c90574a7
Asinkingship <kbosborne1982@gmail.com>
2024-05-18 12:40:22 -0400

    updated as3 fixed

--- scripts/temperature-as3.js
+++ scripts/temperature-as3.js
@@ -2,15 +2,17 @@
 function convertTemp(){
     let userTemp=prompt("Enter temperature")
     let userInput=prompt("Enter c for Celsius or f for Fahrenheit");
-    let c=(userTemp - 32) * 5/9;
-    let f=(userTemp * 9 / 5) + 32;
+    
+    
 
-    if(userInput==c){
+    if(userInput=="c"){
+        let c=(userTemp - 32) * 5/9;
         document.getElementById("tempConversion1").innerHTML= c + " - Celsius";
-    }else if(userInput==f){
-        document.getElementById("tempConversion2").innerHTML= f + " - Fahrenheit";
+    }else if(userInput=="f"){
+        let f=(userTemp * 9 / 5) + 32;  
+        document.getElementById("tempConversion1").innerHTML= f + " - Fahrenheit";
     }else{
-        document.getElementById("tempConversion3").innerHTML= "Invalid Input";
+        document.getElementById("tempConversion1").innerHTML= "Invalid Input";
     }
 }
 
